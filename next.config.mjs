7a3c46409635e12e6793bd5cb8d2421d8518ea96
Zvi <zvi_septon@manulife.ca>
2024-01-31 14:07:49 -0500

    add base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",  // <=== enables static exports
+    basePath: "/public-page",
+    reactStrictMode: true,
 };
 
 export default nextConfig;
